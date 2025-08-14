d7fff8669798c4a92084d1c504e9e8130bcd675e
IRASUBIZA-SALY-NELSON <irasubizasalynelson@gmail.com>
2025-08-14 22:59:37 +0100

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Student from './components/Student'
 import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/student" element={<Student />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
